7069fcf999aded08857307dc8ddd366e0a55f268
Doraemon@012 <neyati28@gmail.com>
2024-05-16 23:28:46 +0530

    redesign homepage

---
+++ src/theme.tsx
@@ -0,0 +1,33 @@
+// theme.ts
+
+const theme = {
+  colors: {
+    lightBackground: "#fcfcfc",
+    darkBackground: "#0f0f0f",
+    primary: "#5ab2ff",
+    secondaryDark: "#a0deff",
+    secondaryLight: "#caf4ff",
+    accent: "#ffdd00",
+    whiteCardOutline: "#9abac2",
+    brightBackground: "#ffffff",
+    textBoxOutline: "#bcc8cb",
+  },
+  borderRadius: {
+    large: 20,
+    medium: 16,
+    small: 8,
+    extraSmall: 4,
+  },
+  padding: {
+    default: 16,
+    small: 8,
+  },
+  fontSize: {
+    textBody: 18,
+    heading: 35,
+    subheading: 30,
+    mediumFont: 24,
+  },
+};
+
+export default theme;
