7996f9609bbadbc054e230cd5279c9b7c6034e9b
Riya Sharma <sharmariya11133@gmail.com>
2024-05-19 16:39:37 +0530

    Fixed

--- src/components/DashBoard/DashBoard.tsx
+++ src/components/DashBoard/DashBoard.tsx
@@ -1,28 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Button, Spinner } from "react-bootstrap";
-import Signup from "../Signup/Signup";
-import CreateEvent from "../Cards/CreateEvent/CreateEvent";
-import ProfilePage from "../ProfilePage/ProfilePage";
-import bannerImage3 from "../image_assets/bannerImage3.png";
-import "./DashBoard.css";
-import EventCard from "../Cards/EventCard/EventCard";
 import { Pagination } from "react-bootstrap";
-
-import {
-  auth,
-  firestore,
-  database,
-  storage,
-  signInWithGooglePopup,
-} from "../../firebaseConf";
-import GoogleButton from "react-google-button";
-import { ref, get, child, set } from "firebase/database";
+import { ref, get, child } from "firebase/database";
 import { Zoom, toast } from "react-toastify";
-import {
-  ref as storageRef,
-  uploadBytes,
-  getDownloadURL,
-} from "firebase/storage";
+import EventCard from "../Cards/EventCard/EventCard";
+import { database } from "../../firebaseConf";
+import "./DashBoard.css";
+
 type Event = {
   id: string;
   title: string;
@@ -40,16 +24,19 @@ const Dashboard = () => {
   const itemsPerPage = 10;
   const [totalPages, setTotalPages] = useState(1);
 
-  const [eventCardsData, setEventCardsData] = useState<Event[]>([]);
+  const [createdEvents, setCreatedEvents] = useState<Event[]>([]);
+  const [joinedEvents, setJoinedEvents] = useState<Event[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber);
   };
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (localStorage.getItem("userUid") == null) {
       window.location.href = "#/";
       toast.warn("You are not signed in", { transition: Zoom });
+      return;
     }
 
     const fetchData = async () => {
@@ -61,42 +48,47 @@ const Dashboard = () => {
         return;
       }
 
-      // userRef is the reference to the user's data in the database
       const userRef = child(usersRef, userUid);
       const snapshot = await get(userRef);
 
       if (snapshot.exists()) {
-        let eventList: any[] = [];
+        let createdEventList: string[] = [];
+        let joinedEventList: string[] = [];
+
         if (snapshot.hasChild("createdEvents")) {
-          eventList = [
-            ...eventList,
-            ...snapshot.val().createdEvents.split(","),
-          ];
+          createdEventList = snapshot.val().createdEvents.split(",");
         }
         if (snapshot.hasChild("registeredEvents")) {
-          const registeredEvents = snapshot.val().registeredEvents.split(",");
-          registeredEvents.forEach((event: string) => {
-            if (!eventList.includes(event)) {
-              eventList.push(event);
-            }
-          });
+          joinedEventList = snapshot.val().registeredEvents.split(",");
         }
-        eventList.forEach((eventId: string) => {
-          const trimmedEventId = eventId.trim();
-          const eventsRef = ref(database, "events");
 
-          const eventRef = child(eventsRef, trimmedEventId);
-          get(eventRef).then((snapshot) => {
-            if (snapshot.exists()) {
-              const event = snapshot.val();
-              setEventCardsData((prev: any[]) => [...prev, event]);
-              setTotalPages(Math.ceil(eventCardsData.length / itemsPerPage));
-              setIsLoading(false);
-            } else {
-              console.log("No data available");
-            }
-          });
-        });
+        const fetchEventDetails = async (
+          eventIdList: string[],
+          setEventList: React.Dispatch<React.SetStateAction<Event[]>>
+        ) => {
+          const eventsRef = ref(database, "events");
+          const eventDetails = await Promise.all(
+            eventIdList.map(async (eventId: string) => {
+              const trimmedEventId = eventId.trim();
+              const eventRef = child(eventsRef, trimmedEventId);
+              const eventSnapshot = await get(eventRef);
+              if (eventSnapshot.exists()) {
+                return eventSnapshot.val();
+              } else {
+                console.log(`Event with ID ${trimmedEventId} does not exist`);
+                return null;
+              }
+            })
+          );
+
+          setEventList(
+            eventDetails.filter((event) => event !== null) as Event[]
+          );
+        };
+
+        await fetchEventDetails(createdEventList, setCreatedEvents);
+        await fetchEventDetails(joinedEventList, setJoinedEvents);
+
         setIsLoading(false);
       } else {
         console.log("No data available");
@@ -106,6 +98,7 @@ const Dashboard = () => {
 
     fetchData();
   }, []);
+
   return (
     <div>
       {isLoading ? (
@@ -123,24 +116,15 @@ const Dashboard = () => {
           >
             Created Events
           </h1>
-
-          {eventCardsData
-            .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
-            .map(
-              (
-                card: {
-                  id: string;
-                  title: string;
-                  description: string;
-                  date: string;
-                  time: string;
-                  tags: string;
-                  banner: string;
-                  host: string;
-                  hostName: string;
-                },
-                index
-              ) => (
+          {createdEvents.length === 0 ? (
+            <p>No created events found.</p>
+          ) : (
+            createdEvents
+              .slice(
+                (currentPage - 1) * itemsPerPage,
+                currentPage * itemsPerPage
+              )
+              .map((card: Event, index) => (
                 <EventCard
                   isValid={false}
                   id={card.id}
@@ -155,8 +139,44 @@ const Dashboard = () => {
                   image={card.banner}
                   hostName={card.hostName}
                 />
+              ))
+          )}
+
+          <h1
+            style={{
+              textAlign: "center",
+              marginBottom: "2em",
+              marginTop: "1em",
+            }}
+          >
+            Joined Events
+          </h1>
+          {joinedEvents.length === 0 ? (
+            <p>No joined events found.</p>
+          ) : (
+            joinedEvents
+              .slice(
+                (currentPage - 1) * itemsPerPage,
+                currentPage * itemsPerPage
               )
-            )}
+              .map((card: Event, index) => (
+                <EventCard
+                  isValid={false}
+                  id={card.id}
+                  key={index}
+                  title={card.title}
+                  description={card.description}
+                  date={card.date}
+                  time={card.time}
+                  tags={card.tags}
+                  host={card.host}
+                  isDashboard={true}
+                  image={card.banner}
+                  hostName={card.hostName}
+                />
+              ))
+          )}
+
           <div style={{ display: "flex", justifyContent: "center" }}>
             <Pagination>
               {[...Array(totalPages)].map((e, i) => (
