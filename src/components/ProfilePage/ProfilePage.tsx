9b5a87f0f24bb5cafd8ddae88e78669a1ca2fd94
Tanay-ErrorCode <codingkaku@gmail.com>
2024-05-23 11:20:13 +0530

    Minor improvements in ProfilePage

--- src/components/ProfilePage/ProfilePage.tsx
+++ src/components/ProfilePage/ProfilePage.tsx
@@ -19,6 +19,7 @@ import { Zoom, toast } from "react-toastify";
 import { useParams } from "react-router-dom";
 import { auth, database } from "../../firebaseConf";
 import { Instagram, Twitter, Facebook } from "@mui/icons-material";
+import { X } from "@mui/icons-material";
 
 interface Event {
   banner: string;
@@ -108,7 +109,6 @@ const ProfilePage = () => {
       const events = await Promise.all(eventPromises);
       return events.filter((event): event is Event => event !== null);
     };
-
     const updateProfileData = (userData: any) => {
       const headline = document.getElementById(
         "headline"
@@ -133,15 +133,53 @@ const ProfilePage = () => {
       headline.innerText = userData.headline || "Developer";
       userName.innerText = userData.name || "Sample User";
       website.innerText = userData.website || "NAN";
-      instagram.href = userData.instagram || "";
-      instagram.style.opacity = userData.instagram ? "1.0" : "0.5";
-      instagram.style.pointerEvents = userData.instagram ? "auto" : "none";
-      twitter.href = userData.twitter || "";
-      twitter.style.opacity = userData.twitter ? "1.0" : "0.5";
-      twitter.style.pointerEvents = userData.twitter ? "auto" : "none";
-      facebook.href = userData.facebook || "";
-      facebook.style.opacity = userData.facebook ? "1.0" : "0.5";
-      facebook.style.pointerEvents = userData.facebook ? "auto" : "none";
+
+      const isValidUrl = (url: string) => {
+        return url.startsWith("https://");
+      };
+
+      if (
+        userData.instagram &&
+        isValidUrl(userData.instagram.trim()) &&
+        userData.instagram != "https://" &&
+        userData.instagram != ""
+      ) {
+        instagram.href = userData.instagram;
+        instagram.style.opacity = "1.0";
+        instagram.style.pointerEvents = "auto";
+      } else {
+        instagram.style.opacity = "0.5";
+        instagram.style.pointerEvents = "none";
+      }
+
+      if (
+        userData.twitter &&
+        isValidUrl(userData.twitter.trim()) &&
+        userData.twitter != "https://" &&
+        userData.twitter != ""
+      ) {
+        twitter.href = userData.twitter;
+        twitter.style.opacity = "1.0";
+        twitter.style.pointerEvents = "auto";
+      } else {
+        twitter.style.opacity = "0.5";
+        twitter.style.pointerEvents = "none";
+      }
+
+      if (
+        userData.facebook &&
+        isValidUrl(userData.facebook.trim()) &&
+        userData.facebook != "https://" &&
+        userData.facebook != ""
+      ) {
+        facebook.href = userData.facebook;
+        facebook.style.opacity = "1.0";
+        facebook.style.pointerEvents = "auto";
+      } else {
+        facebook.style.opacity = "0.5";
+        facebook.style.pointerEvents = "none";
+      }
+
       profileBanner.src = userData.banner || bannerImage;
       profileImage.src = userData.pic || default_user;
 
@@ -178,6 +216,7 @@ const ProfilePage = () => {
                   className="profile-banner"
                   fluid
                   id="profile-banner"
+                  style={{ pointerEvents: "none", borderRadius: "16px" }}
                 />
                 <div className="profile-image-overlay position-absolute top-100 start-50 translate-middle">
                   <Image
@@ -255,7 +294,7 @@ const ProfilePage = () => {
                         target="_blank"
                         rel="noopener noreferrer"
                       >
-                        <Twitter className="twitter-icon" />
+                        <X className="x-icon" />
                       </a>
                       <span className="social-media-label" />
                     </div>
