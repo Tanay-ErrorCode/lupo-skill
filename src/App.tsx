7f5e82a00de91a5935be7bbbf11b74cc9b8f2005
Annu Kumari <108427028+Annu117@users.noreply.github.com>
2024-05-27 06:19:18 +0530

    Update

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import EventList from "./components/EventList/EventList";
+import { Routes, Route, HashRouter } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import Dashboard from "./components/DashBoard/DashBoard";
+import ProfilePage from "./components/ProfilePage/ProfilePage";
+import EventDetails from "./components/EventDetails/EventDetails";
+import Signup from "./components/Signup/Signup";
+import { NotificationsPannel } from "./components/NotificationsPannel";
+import ErrorPage from "./components/ErrorPage/404ErrorPage";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <NotificationsPannel />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/events" element={<EventList />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile/:id" element={<ProfilePage />} />
+          <Route path="/eventDetails/:id" element={<EventDetails />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </HashRouter>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
