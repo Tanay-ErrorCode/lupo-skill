4217a075d5648b28f54144aa3f1e89027fa1b660
Tanay-ErrorCode <codingkaku@gmail.com>
2024-06-08 12:07:14 +0530

    resolved conflicts

--- src/firebaseConf.ts
+++ src/firebaseConf.ts
@@ -14,7 +14,7 @@ import { getStorage } from "firebase/storage";
 import { Zoom, toast } from "react-toastify";
 
 const firebaseConfig = {
-  //enter the config details from firebase
+  // firebase config
 };
 
 const app = firebase.initializeApp(firebaseConfig);
