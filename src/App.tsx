aaade64f8c641a4635be9f29bb7ed4bc36b035df
Minav Karia <119132631+MinavKaria@users.noreply.github.com>
2024-06-26 19:36:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import EventList from "./components/EventList/EventList";
-import { Routes, Route, HashRouter } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import Dashboard from "./components/DashBoard/DashBoard";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
 import EventDetails from "./components/EventDetails/EventDetails";
-
 import Signup from "./components/Signup/Signup";
 import { NotificationsPannel } from "./components/NotificationsPannel";
 import ErrorPage from "./components/ErrorPage/404ErrorPage";
@@ -19,50 +20,78 @@ import CreateEvent from "./components/Cards/CreateEvent/CreateEvent";
 import ArticlesHomepage from "./components/Article/ArticlesHomepage";
 import ArticleWritingPage from "./components/Article/ArticleWritingPage";
 
+const Layout = () => (
+  <>
+    <NotificationsPannel />
+    <NavBar />
+    <ScrollToTop />
+    <Outlet />
+    <Footer />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <HomePage />
+            <PageTitle title="Lupo Skill" />
+          </>
+        ),
+      },
+      {
+        path: "/events",
+        element: (
+          <>
+            <EventList />
+            <PageTitle title="Events | Lupo Skill" />
+          </>
+        ),
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <>
+            <Dashboard />
+            <PageTitle title="Dashboard | Lupo Skill" />
+          </>
+        ),
+      },
+      {
+        path: "/createEvent",
+        element: <CreateEvent />,
+      },
+      {
+        path: "/profile/:id",
+        element: <ProfilePage />,
+      },
+      {
+        path: "/eventDetails/:id",
+        element: <EventDetails />,
+      },
+      {
+        path: "/article",
+        element: <ArticlesHomepage />,
+      },
+      {
+        path: "/article/write",
+        element: <ArticleWritingPage />,
+      },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div className="App">
-      <NotificationsPannel />
-      <HashRouter>
-        <NavBar />
-        <ScrollToTop />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <HomePage />
-                <PageTitle title="Lupo Skill" />
-              </>
-            }
-          />
-          <Route
-            path="/events"
-            element={
-              <>
-                <EventList /> <PageTitle title="Events | Lupo Skill" />
-              </>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <>
-                <Dashboard /> <PageTitle title="Dashboard | Lupo Skill" />
-              </>
-            }
-          />
-          <Route path="/createEvent" element={<CreateEvent />} />
-          <Route path="/profile/:id" element={<ProfilePage />} />
-          <Route path="/eventDetails/:id" element={<EventDetails />} />
-          <Route path="*" element={<ErrorPage />} />
-          <Route path="/article" element={<ArticlesHomepage />} />
-          <Route path="/article/write" element={<ArticleWritingPage />} />
-        </Routes>
-      </HashRouter>
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
