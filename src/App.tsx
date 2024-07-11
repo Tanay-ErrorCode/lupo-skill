e70c489913fbcd1f563bacfe1e4861e752ab360f
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-07-11 23:14:29 +0530

    preloader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import preloader from "./preloader.gif";
 import NavBar from "./components/NavBar/NavBar";
 import EventList from "./components/EventList/EventList";
 import { Routes, Route, HashRouter } from "react-router-dom";
@@ -9,7 +9,6 @@ import Dashboard from "./components/DashBoard/DashBoard";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
 import EventDetails from "./components/EventDetails/EventDetails";
 
-import Signup from "./components/Signup/Signup";
 import { NotificationsPannel } from "./components/NotificationsPannel";
 import ErrorPage from "./components/ErrorPage/404ErrorPage";
 import Footer from "./components/Footer/Footer";
@@ -21,64 +20,82 @@ import ArticleWritingPage from "./components/Article/ArticleWritingPage";
 import ArticlePage from "./components/Article/ArticlePage";
 
 function App() {
+  const [screenLoading, setScreenLoading] = useState(false);
+
+  useEffect(() => {
+    setScreenLoading(true);
+    setTimeout(() => {
+      setScreenLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
-      <NotificationsPannel />
-      <HashRouter>
-        <NavBar />
-        <ScrollToTop />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <HomePage />
-                <PageTitle title="Lupo Skill" />
-              </>
-            }
-          />
-          <Route
-            path="/events"
-            element={
-              <>
-                <EventList /> <PageTitle title="Events | Lupo Skill" />
-              </>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <>
-                <Dashboard /> <PageTitle title="Dashboard | Lupo Skill" />
-              </>
-            }
-          />
-          <Route path="/createEvent" element={<CreateEvent />} />
-          <Route path="/profile/:id" element={<ProfilePage />} />
-          <Route path="/eventDetails/:id" element={<EventDetails />} />
-          <Route path="*" element={<ErrorPage />} />
+      {screenLoading ? (
+        <div className="preloader">
+          <img src={preloader} alt="Loading..." />
+        </div>
+      ) : (
+        <div>
+          <NotificationsPannel />
+          <HashRouter>
+            <NavBar />
+            <ScrollToTop />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <HomePage />
+                    <PageTitle title="Lupo Skill" />
+                  </>
+                }
+              />
+              <Route
+                path="/events"
+                element={
+                  <>
+                    <EventList /> <PageTitle title="Events | Lupo Skill" />
+                  </>
+                }
+              />
+              <Route
+                path="/dashboard"
+                element={
+                  <>
+                    <Dashboard /> <PageTitle title="Dashboard | Lupo Skill" />
+                  </>
+                }
+              />
+              <Route path="/createEvent" element={<CreateEvent />} />
+              <Route path="/profile/:id" element={<ProfilePage />} />
+              <Route path="/eventDetails/:id" element={<EventDetails />} />
+              <Route path="*" element={<ErrorPage />} />
 
-          <Route path="/article/:id" element={<ArticlePage />} />
-          <Route
-            path="/article"
-            element={
-              <>
-                <ArticlesHomepage /> <PageTitle title="Articles | Lupo Skill" />
-              </>
-            }
-          />
-          <Route
-            path="/article/write"
-            element={
-              <>
-                <ArticleWritingPage />{" "}
-                <PageTitle title="Create Article | Lupo Skill" />
-              </>
-            }
-          />
-        </Routes>
-      </HashRouter>
-      <Footer />
+              <Route path="/article/:id" element={<ArticlePage />} />
+              <Route
+                path="/article"
+                element={
+                  <>
+                    <ArticlesHomepage />{" "}
+                    <PageTitle title="Articles | Lupo Skill" />
+                  </>
+                }
+              />
+              <Route
+                path="/article/write"
+                element={
+                  <>
+                    <ArticleWritingPage />{" "}
+                    <PageTitle title="Create Article | Lupo Skill" />
+                  </>
+                }
+              />
+            </Routes>
+          </HashRouter>
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
