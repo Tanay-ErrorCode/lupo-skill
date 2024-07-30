704a71d241e0ca2310f490dd10e7806038436527
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-07-30 06:41:05 +0530

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route, HashRouter, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import EventList from "./components/EventList/EventList";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 import HomePage from "./components/HomePage/HomePage";
 import Dashboard from "./components/DashBoard/DashBoard";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
 import EventDetails from "./components/EventDetails/EventDetails";
-
 import Signup from "./components/Signup/Signup";
 import { NotificationsPannel } from "./components/NotificationsPannel";
 import ErrorPage from "./components/ErrorPage/404ErrorPage";
-import Footer from "./components/Footer/Footer";
 import PageTitle from "./utils/PageTitle";
 import { ScrollToTop } from "./components/Scrolltotop";
 import CreateEvent from "./components/Cards/CreateEvent/CreateEvent";
@@ -21,69 +17,80 @@ import ArticleWritingPage from "./components/Article/ArticleWritingPage";
 import ArticlePage from "./components/Article/ArticlePage";
 import ArticleEditingPage from "./components/Article/ArticleEditingPage";
 import MeetingPage from "./components/Meetings/MeetingPage";
+import EventList from "./components/EventList/EventList";
 
 function App() {
   return (
     <div className="App">
       <NotificationsPannel />
       <HashRouter>
-        <NavBar />
         <ScrollToTop />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <HomePage />
-                <PageTitle title="Lupo Skill" />
-              </>
-            }
-          />
-          <Route
-            path="/events"
-            element={
-              <>
-                <EventList /> <PageTitle title="Events | Lupo Skill" />
-              </>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <>
-                <Dashboard /> <PageTitle title="Dashboard | Lupo Skill" />
-              </>
-            }
-          />
-          <Route path="/createEvent" element={<CreateEvent />} />
-          <Route path="/profile/:id" element={<ProfilePage />} />
-          <Route path="/eventDetails/:id" element={<EventDetails />} />
-          <Route path="*" element={<ErrorPage />} />
-
-          <Route path="/article/:id" element={<ArticlePage />} />
-          <Route
-            path="/article"
-            element={
-              <>
-                <ArticlesHomepage /> <PageTitle title="Articles | Lupo Skill" />
-              </>
-            }
-          />
-          <Route
-            path="/article/write"
-            element={
-              <>
-                <ArticleWritingPage />{" "}
-                <PageTitle title="Create Article | Lupo Skill" />
-              </>
-            }
-          />
-          <Route path="/edit-article/:id" element={<ArticleEditingPage />} />
-          <Route path="/meeting/:id" element={<MeetingPage />} />
-        </Routes>
+        <MainContent />
       </HashRouter>
-      <Footer />
     </div>
+  );
+}
+
+function MainContent() {
+  const location = useLocation();
+  const hideNavBarAndFooter = location.pathname.includes("/meeting");
+
+  return (
+    <>
+      {!hideNavBarAndFooter && <NavBar />}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <HomePage />
+              <PageTitle title="Lupo Skill" />
+            </>
+          }
+        />
+        <Route
+          path="/events"
+          element={
+            <>
+              <EventList /> <PageTitle title="Events | Lupo Skill" />
+            </>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              <Dashboard /> <PageTitle title="Dashboard | Lupo Skill" />
+            </>
+          }
+        />
+        <Route path="/createEvent" element={<CreateEvent />} />
+        <Route path="/profile/:id" element={<ProfilePage />} />
+        <Route path="/eventDetails/:id" element={<EventDetails />} />
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/article/:id" element={<ArticlePage />} />
+        <Route
+          path="/article"
+          element={
+            <>
+              <ArticlesHomepage /> <PageTitle title="Articles | Lupo Skill" />
+            </>
+          }
+        />
+        <Route
+          path="/article/write"
+          element={
+            <>
+              <ArticleWritingPage />{" "}
+              <PageTitle title="Create Article | Lupo Skill" />
+            </>
+          }
+        />
+        <Route path="/edit-article/:id" element={<ArticleEditingPage />} />
+        <Route path="/meeting/:id" element={<MeetingPage />} />
+      </Routes>
+      {!hideNavBarAndFooter && <Footer />}
+    </>
   );
 }
 
--- src/components/Meetings/MeetingPage.tsx
+++ src/components/Meetings/MeetingPage.tsx
@@ -8,21 +8,12 @@ import MicOffIcon from "@mui/icons-material/MicOff";
 import VideocamIcon from "@mui/icons-material/Videocam";
 import VideocamOffIcon from "@mui/icons-material/VideocamOff";
 import ScreenShareIcon from "@mui/icons-material/ScreenShare";
-import StopScreenShareIcon from "@mui/icons-material/StopScreenShare";
 import CallEndIcon from "@mui/icons-material/CallEnd";
 import ChatIcon from "@mui/icons-material/Chat";
 import InfoIcon from "@mui/icons-material/Info";
 import ParticipantsIcon from "@mui/icons-material/Group";
-import LockIcon from "@mui/icons-material/Lock";
 import SendIcon from "@mui/icons-material/Send";
-import {
-  AppBar,
-  Box,
-  Drawer,
-  Toolbar,
-  Typography,
-  TextField,
-} from "@mui/material";
+import { AppBar, Toolbar, Typography, TextField } from "@mui/material";
 import "./MeetingPage.css";
 
 const MeetingPage: React.FC = () => {
@@ -73,117 +64,113 @@ const MeetingPage: React.FC = () => {
   };
 
   return (
-    <div className="meeting-page">
-      <div className="meeting-content">
-        <div className="mute_icon_meeting">
-          <IconButton
-            className="icon_back"
-            onClick={toggleAudio}
-            color="inherit"
-          >
-            {isAudioOn ? (
-              <MicIcon style={{ fontSize: "18px" }} />
-            ) : (
-              <MicOffIcon style={{ fontSize: "18px" }} />
-            )}
-          </IconButton>
-        </div>
-        {userData ? (
-          <div style={{ textAlign: "center" }}>
-            {isVideoOn ? (
-              <img src={userData.pic} alt="Profile" className="profile-pic" />
-            ) : (
-              <VideocamOffIcon style={{ fontSize: "150px" }} />
-            )}
-            <h5 className="user-info">{userData.name}</h5>
-          </div>
-        ) : (
-          <div>Loading...</div>
-        )}
-
-        <Drawer
-          anchor="right"
-          open={isChatOpen}
-          onClose={toggleChatDrawer}
-          className="chat-drawer"
-        >
-          <div className="chat-content">
-            <h3>Chat</h3>
-            <div className="chat-input-container">
-              <TextField
-                value={chatMessage}
-                rows={1}
-                onChange={(e) => setChatMessage(e.target.value)}
-                placeholder="Type message here..."
-                fullWidth
-              />
-              <IconButton
-                className="send_button_chat"
-                onClick={handleSendMessage}
-              >
-                <SendIcon />
-              </IconButton>
-            </div>
-          </div>
-        </Drawer>
-      </div>
-
-      <div className="bottom-navbar">
-        <Toolbar className="toolbar">
-          <div className="left-icon-div">
-            <Typography
-              variant="body1"
-              color="inherit"
-              style={{ marginRight: "auto" }}
-            >
-              {new Date().toLocaleTimeString()} | {id}
-            </Typography>
-          </div>
-          <div className="center-icon-div">
+    <div className="meeting-page_main">
+      <div className={`meeting-page ${isChatOpen ? "shifted" : ""}`}>
+        <div className="meeting-content">
+          <div className={`mute_icon_meeting ${isChatOpen ? "shifted" : ""}`}>
             <IconButton
               className="icon_back"
               onClick={toggleAudio}
               color="inherit"
             >
               {isAudioOn ? (
-                <MicIcon style={{ fontSize: "26px" }} />
+                <MicIcon style={{ fontSize: "18px" }} />
               ) : (
-                <MicOffIcon style={{ fontSize: "26px" }} />
+                <MicOffIcon style={{ fontSize: "18px" }} />
               )}
             </IconButton>
-            <IconButton
-              className="icon_back"
-              onClick={toggleVideo}
-              color="inherit"
-            >
+          </div>
+          {userData ? (
+            <div style={{ textAlign: "center" }}>
               {isVideoOn ? (
-                <VideocamIcon style={{ fontSize: "26px" }} />
+                <img src={userData.pic} alt="Profile" className="profile-pic" />
               ) : (
-                <VideocamOffIcon style={{ fontSize: "26px" }} />
+                <VideocamOffIcon style={{ fontSize: "150px" }} />
               )}
-            </IconButton>
-            <IconButton className="icon_back" color="inherit">
-              <ScreenShareIcon style={{ fontSize: "26px" }} />
-            </IconButton>
-            <IconButton className="icon_back red" color="inherit">
-              <CallEndIcon />
-            </IconButton>
-          </div>
-          <div>
-            <IconButton color="inherit">
-              <InfoIcon style={{ fontSize: "26px" }} />
-            </IconButton>
-            <IconButton color="inherit">
-              <ChatIcon
-                style={{ fontSize: "26px" }}
-                onClick={toggleChatDrawer}
-              />
-            </IconButton>
-            <IconButton color="inherit">
-              <ParticipantsIcon style={{ fontSize: "26px" }} />
-            </IconButton>
-          </div>
-        </Toolbar>
+              <h5 className="user-info">{userData.name}</h5>
+            </div>
+          ) : (
+            <div>Loading...</div>
+          )}
+        </div>
+        <div className={`bottom-navbar ${isChatOpen ? "shifted" : ""}`}>
+          <Toolbar className="toolbar">
+            <div className="left-icon-div">
+              <Typography
+                variant="body1"
+                color="inherit"
+                style={{ marginRight: "auto" }}
+              >
+                {new Date().toLocaleTimeString()} | {id}
+              </Typography>
+            </div>
+            <div className="center-icon-div">
+              <IconButton
+                className="icon_back"
+                onClick={toggleAudio}
+                color="inherit"
+              >
+                {isAudioOn ? (
+                  <MicIcon style={{ fontSize: "26px" }} />
+                ) : (
+                  <MicOffIcon style={{ fontSize: "26px" }} />
+                )}
+              </IconButton>
+              <IconButton
+                className="icon_back"
+                onClick={toggleVideo}
+                color="inherit"
+              >
+                {isVideoOn ? (
+                  <VideocamIcon style={{ fontSize: "26px" }} />
+                ) : (
+                  <VideocamOffIcon style={{ fontSize: "26px" }} />
+                )}
+              </IconButton>
+              <IconButton className="icon_back" color="inherit">
+                <ScreenShareIcon style={{ fontSize: "26px" }} />
+              </IconButton>
+              <IconButton className="icon_back red" color="inherit">
+                <CallEndIcon />
+              </IconButton>
+            </div>
+            <div className="right-icon-div">
+              <IconButton color="inherit">
+                <InfoIcon style={{ fontSize: "26px" }} />
+              </IconButton>
+              <IconButton color="inherit" className="chat_icon">
+                <ChatIcon
+                  style={{ fontSize: "26px" }}
+                  onClick={toggleChatDrawer}
+                />
+              </IconButton>
+              <IconButton color="inherit">
+                <ParticipantsIcon style={{ fontSize: "26px" }} />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </div>
+      </div>
+      <div className={`chat-drawer ${isChatOpen ? "open" : ""}`}>
+        <button className="close-btn" onClick={toggleChatDrawer}>
+          ×
+        </button>
+        <div className="chat-content">
+          <h3>Chat</h3>
+          {/* Display chat messages here */}
+        </div>
+        <div className="chat-input-container">
+          <TextField
+            label="Type a message"
+            variant="outlined"
+            fullWidth
+            value={chatMessage}
+            onChange={(e) => setChatMessage(e.target.value)}
+          />
+          <IconButton onClick={handleSendMessage}>
+            <SendIcon className="send_button_chat" />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
