1eccfe8726c441db8458448fefae8c80a2b20516
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-08-01 06:32:38 +0530

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Routes, Route, HashRouter, useLocation } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/Footer";
+import EventList from "./components/EventList/EventList";
+import { Routes, Route, HashRouter, useLocation } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import Dashboard from "./components/DashBoard/DashBoard";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
@@ -9,6 +11,7 @@ import EventDetails from "./components/EventDetails/EventDetails";
 import Signup from "./components/Signup/Signup";
 import { NotificationsPannel } from "./components/NotificationsPannel";
 import ErrorPage from "./components/ErrorPage/404ErrorPage";
+import Footer from "./components/Footer/Footer";
 import PageTitle from "./utils/PageTitle";
 import { ScrollToTop } from "./components/Scrolltotop";
 import CreateEvent from "./components/Cards/CreateEvent/CreateEvent";
@@ -17,27 +20,26 @@ import ArticleWritingPage from "./components/Article/ArticleWritingPage";
 import ArticlePage from "./components/Article/ArticlePage";
 import ArticleEditingPage from "./components/Article/ArticleEditingPage";
 import MeetingPage from "./components/Meetings/MeetingPage";
-import EventList from "./components/EventList/EventList";
 
 function App() {
   return (
     <div className="App">
       <NotificationsPannel />
       <HashRouter>
-        <ScrollToTop />
-        <MainContent />
+        <AppContent />
       </HashRouter>
     </div>
   );
 }
 
-function MainContent() {
+function AppContent() {
   const location = useLocation();
-  const hideNavBarAndFooter = location.pathname.includes("/meeting");
+  const isMeetingRoute = location.pathname.startsWith("/meeting/");
 
   return (
     <>
-      {!hideNavBarAndFooter && <NavBar />}
+      {!isMeetingRoute && <NavBar />}
+      <ScrollToTop />
       <Routes>
         <Route
           path="/"
@@ -89,7 +91,7 @@ function MainContent() {
         <Route path="/edit-article/:id" element={<ArticleEditingPage />} />
         <Route path="/meeting/:id" element={<MeetingPage />} />
       </Routes>
-      {!hideNavBarAndFooter && <Footer />}
+      {!isMeetingRoute && <Footer />}
     </>
   );
 }
